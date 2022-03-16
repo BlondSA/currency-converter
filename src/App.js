4a3ed3d37e5d3d166e7ee7e4626487326ace155e
St.Anton <stryuchkov.anton@gmail.com>
2022-03-16 15:38:39 +0300

    Turns the project into a Counter and Adds Hooks, adds own hooks

--- src/App.js
+++ src/App.js
@@ -1,44 +1,82 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
-const App = (props) => {
-    const [currency, setCurrency] = useState("USD");
-    const [value, setValue] = useState(null);
-    const [inputValue, setInputValue] = useState(0);
+const useCounter = (initial) => {
+    const [counter, setCounter] = useState(initial);
 
     useEffect(() => {
-        fetch("https://www.cbr-xml-daily.ru/daily_json.js")
-            .then((response) => response.json())
-            .then((data) => {
-                const count = data.Valute[currency].Value;
-                setValue((inputValue * count).toFixed(2));
+        fetch(
+            "https://www.random.org/integers/?num=1&min=-50&max=50&col=1&base=10&format=plain&rnd=new"
+        )
+            .then((response) => response.text())
+            .then((response) => {
+                setCounter(response);
+            })
+            .catch((error) => {
+                throw new Error(error);
             });
-    }, [currency, inputValue]);
+    }, []);
+
+    const incCounter = () => {
+        if (counter < 50) {
+            setCounter((counter) => counter + 1);
+        }
+    };
+
+    const decCounter = () => {
+        if (counter > -50) {
+            setCounter((counter) => counter - 1);
+        }
+    };
+
+    const rndCounter = () => {
+        setCounter(+(Math.random() * (50 - -50) + -50).toFixed(0));
+    };
+
+    const resetCounter = () => {
+        setCounter(0);
+    };
+
+    return { counter, incCounter, decCounter, rndCounter, resetCounter };
+};
+
+const Counter = () => {
+    const { counter, incCounter, decCounter, rndCounter, resetCounter } =
+        useCounter("");
 
     return (
-        <div className="app">
-            <div className="wrapper-valute">
-                <span>{currency}</span>
-                <span>RUB</span>
-            </div>
-            <div className="wrapper-field">
-                <input
-                    value={inputValue}
-                    className="input"
-                    type="number"
-                    placeholder=""
-                    autofocus
-                    onChange={(e) => setInputValue(e.target.value)}
-                />
-                <div className="counter">{value}</div>
-            </div>
-
+        <div className="component">
+            <div className="counter">{counter}</div>
             <div className="controls">
-                <button onClick={() => setCurrency("USD")}>USD</button>
-                <button onClick={() => setCurrency("EUR")}>EUR</button>
-                <button onClick={() => setCurrency("CNY")}>CNY</button>
+                <button onClick={incCounter}>INC</button>
+                <button onClick={decCounter}>DEC</button>
+                <button onClick={rndCounter}>RND</button>
+                <button onClick={resetCounter}>RESET</button>
             </div>
         </div>
+    );
+};
+
+const RndCounter = () => {
+    const { counter, rndCounter, resetCounter } = useCounter("");
+
+    return (
+        <div className="component">
+            <div className="counter">{counter}</div>
+            <div className="controls">
+                <button onClick={rndCounter}>RND</button>
+                <button onClick={resetCounter}>RESET</button>
+            </div>
+        </div>
+    );
+};
+
+const App = () => {
+    return (
+        <>
+            <Counter counter={0} />
+            <RndCounter counter={0} />
+        </>
     );
 };
 
